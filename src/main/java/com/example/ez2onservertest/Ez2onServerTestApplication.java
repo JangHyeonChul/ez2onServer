97931ce95ea178157507708fe4a346e9d01c440c
Hyeonchul <wkdgus1136@naver.com>
2023-06-14 01:44:56 +0900

    Initial commit

---
+++ src/main/java/com/example/ez2onservertest/Ez2onServerTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.ez2onservertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ez2onServerTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ez2onServerTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ez2onservertest/Ez2onServerTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ez2onservertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ez2onServerTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
